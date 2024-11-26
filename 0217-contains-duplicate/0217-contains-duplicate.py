f2024111e0a3356a14ae88b16f1ad0f9470445ce
Kirthikraj Kamaraj <74924366+Kirthik1104@users.noreply.github.com>
2024-11-26 18:50:23 -0500

    Time: 55 ms (5.02%), Space: 34.5 MB (8.79%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,22 +1,29 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        # hashmap={}
+        """
+        Using Hashmap
+        1) 1 for loop to map values with no. of occurence
+        2) 2nd for loop to acces the value whose occurece is greate than 2
+        """
+        hashmap={}
+        for num in nums:
+            if num in hashmap:
+                hashmap[num]+=1
+            else:
+                hashmap[num]=1
+        print(hashmap)
+        for val in hashmap.values():
+            if val>1:
+                return True
+        return False
 
-        # for num in nums:
-        #     if num in hashmap:
-        #         hashmap[num]+=1
-        #     else:
-        #         hashmap[num]=1
-        # print(hashmap)
-        # for val in hashmap.values():
-        #     if val>1:
-        #         return True
-        # return False
 
+        """
+        Using Hashset
+        1) Using 1 for loop to add unique values to hashset and if same values repeat return true immediately
+        2) If same values not present till the end, return false at the end of loop
+        """
         hashset=set()
-
-
-
         for num in nums:
             if num in hashset:
                 return True
