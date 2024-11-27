4227c8ef843dbe3755d13cf872508f38fa1cf5e9
Kirthikraj Kamaraj <74924366+Kirthik1104@users.noreply.github.com>
2024-11-27 00:32:18 -0500

    Time: 19 ms (36.85%), Space: 22.4 MB (6.96%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -11,18 +11,18 @@ class Solution:
         Explanation: For each string, we sort it, which takes O(K log K) time. We then use the sorted string as a key in the dictionary. The sorting step is what introduces the logarithmic factor, making this approach less efficient than character counting.
                 """
 
-        freq={}
-        for char in strs: # go throug each char in list
+        # freq={}
+        # for char in strs: # go throug each char in list
 
-            sorted_char=sorted(char)  # sort each string. eat becomes aet
+        #     sorted_char=sorted(char)  # sort each string. eat becomes aet
 
-            key=tuple(sorted_char)  #convert string to tuple so that it becoomes immutable 
+        #     key=tuple(sorted_char)  #convert string to tuple so that it becoomes immutable 
 
-            if  key not in freq:  # check if key exists. if not then create and assign original string in list
-                freq[key]=[char]
-            else:
-                freq[key].append(char) # if already exists. append it
-        return list(freq.values())   # return it in the form of list fo values 
+        #     if  key not in freq:  # check if key exists. if not then create and assign original string in list
+        #         freq[key]=[char]
+        #     else:
+        #         freq[key].append(char) # if already exists. append it
+        # return list(freq.values())   # return it in the form of list fo values 
 
        
 
@@ -34,17 +34,17 @@ class Solution:
         # K: Average length of the strings.
         # Explanation: For each string, we count the occurrences of each character in linear time (O(K)) and store the results in a dictionary. The dictionary operations (insertion and lookup) are average O(1).
         # """
-        # mydict=defaultdict(list)
+        mydict=defaultdict(list)
 
-        # for chars in strs:
-        #     count=[0]*26
+        for chars in strs:
+            count=[0]*26
             
-        #     for char in chars:
-        #         count[ord(char)-ord('a')]+=1
+            for char in chars:
+                count[ord(char)-ord('a')]+=1
             
-        #     mydict[tuple(count)].append(chars)
+            mydict[tuple(count)].append(chars)
             
-        # return ([values for values in mydict.values()])
+        return ([values for values in mydict.values()])
 
       
 
