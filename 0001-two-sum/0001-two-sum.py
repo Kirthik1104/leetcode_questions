b866df3738ce5187054eebeced50752cba226ba7
Kirthikraj Kamaraj <74924366+Kirthik1104@users.noreply.github.com>
2024-11-26 22:45:34 -0500

    Update 0001-two-sum.py
    added the test cases, time and space complexity

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -15,34 +15,38 @@ class Solution:
 
             # else iff  diff not in hashmap, store the current number with its index in the hash map
             hashmap[num]=i
-       
-# Code Walkthrough Using Test Case nums2 = [3, 4, 6, 2, 10], target2 = 8:
 
-# Step 0:
-# --------
-# Current number: 3
-# Complement: 8 - 3 = 5
-# Check: Is 5 in the hash map? No
-# Add 3 to the hash map: {3: 0}
+Final Complexities
+Time Complexity: O(n) (One pass through the array with constant-time operations per element)
+Space Complexity: O(n) (Hash map storage for up to n elements)
+------------------------------------------------------------------------------------------------------------------------
+Code Walkthrough Using Test Case nums2 = [3, 4, 6, 2, 10], target2 = 8:
 
-# Step 1:
-# -------
-# Current number: 4
-# Complement: 8 - 4 = 4
-# Check: Is 4 in the hash map? No
-# Add 4 to the hash map: {3: 0, 4: 1}
+Step 0:
+--------
+Current number: 3
+Complement: 8 - 3 = 5
+Check: Is 5 in the hash map? No
+Add 3 to the hash map: {3: 0}
 
-# Step 2:
-# -------
-# Current number: 6
-# Complement: 8 - 6 = 2
-# Check: Is 2 in the hash map? No
-# Add 6 to the hash map: {3: 0, 4: 1, 6: 2}
+Step 1:
+-------
+Current number: 4
+Complement: 8 - 4 = 4
+Check: Is 4 in the hash map? No
+Add 4 to the hash map: {3: 0, 4: 1}
 
-# Step 3:
-# -------
-# Current number: 2
-# Complement: 8 - 2 = 6
-# Check: Is 6 in the hash map? Yes, at index 2
-# Return: [2, 3]
+Step 2:
+-------
+Current number: 6
+Complement: 8 - 6 = 2
+Check: Is 2 in the hash map? No
+Add 6 to the hash map: {3: 0, 4: 1, 6: 2}
+
+Step 3:
+-------
+Current number: 2
+Complement: 8 - 2 = 6
+Check: Is 6 in the hash map? Yes, at index 2
+Return: [2, 3]
         
