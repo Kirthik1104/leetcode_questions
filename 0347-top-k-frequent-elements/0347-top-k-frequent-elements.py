2a1b420c295fe1d7c8d75ce791823877bfa1dcdf
Kirthikraj Kamaraj <74924366+Kirthik1104@users.noreply.github.com>
2024-11-27 18:56:21 -0500

    Update 0347-top-k-frequent-elements.py
    bucket fill
    heap
    sorting
    
    added time complexity, space

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -2,7 +2,7 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
 
         """
-        Using Bucket fill logic (optiaml)-->without soring
+        Using Bucket fill logic (optiaml)-->without sorting
         ----------------------
         Short Summary
         Time Complexity: O(n)
@@ -36,9 +36,42 @@ class Solution:
 
 
 
+          """
+        Using heap (more optiaml than sorting slightly less than bucket fill logic)-->without sorting
+        ----------------------
+     Time Complexity:
+     1)  Building the frequency map:
+            Iterating through n elements in the input list to calculate frequencies.
+            Time Complexity: O(n)
+            
+    2) Heap operations for the top k elements:
+            Insertions or replacements in the heap take O(log k) per element.
+            We process all n elements, performing O(log k) operations for each.
+            Time Complexity: O(n log k)
+            
+    3) Extracting the top k elements:
+    
+            Once the heap is built, extracting all k elements takes O(k log k).
+            However, this step is often dominated by the previous step (O(n log k)), so it is negligible.
+            
+    4) Total Time Complexity:
+            O(n log k)
+        """
 
-
-
+        import heapq
+        freq={}
+        for num in nums:
+            if num in freq:
+                freq[num]+=1
+            else:
+                freq[num]=1
+        min_heap=[]
+        
+        for num, count in freq.items():   # get the count associated with the number
+            heapq.heappush(min_heap, (count, num))  # In the min_heap store the count with number (count, num). 
+            if len(min_heap)>k:                  # if the size of min_heap exceeds length of k heappop removes minimum count and number combination from min heap
+                heapq.heappop(min_heap)
+        print([num for count, num in min_heap])  # in the end you will the count associated number  inside min_heap
 
 
 
