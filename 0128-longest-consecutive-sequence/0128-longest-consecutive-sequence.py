8f20213aa4f5ca23b17da05592e8e76f1cb320bc
Kirthikraj Kamaraj <74924366+Kirthik1104@users.noreply.github.com>
2024-11-28 11:26:48 -0500

    Update 0128-longest-consecutive-sequence.py
    added brute and optimal with both the code

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,7 +1,15 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         
-      
+         """
+        Overall Time Complexity:
+        -----------------------------
+        The overall time complexity of the algorithm can be expressed as:
+        - O(n) for building the hashset
+        - O(n) for iterating through nums
+        - O(k) for the inner while loop, where k is the total count of unique consecutive numbers processed.
+        Since each number is effectively processed once, the total operations will sum up to O(n) for both loops, resulting in an overall time complexity of O(n).
+        """
         if len(nums)==0:    # if nums is [] or [0] return 0
             return 0
 
@@ -25,45 +33,27 @@ class Solution:
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         """
         Overall Time Complexity: The overall time complexity of the algorithm is dominated by the sorting step, resulting in O(n log n)
         """
-        # if len(nums)==0:
-        #     return 0
-        # nums.sort()
+        if len(nums)==0:
+            return 0
+        nums.sort()
 
 
-        # maxi=1
-        # n=len(nums)   # sort the arry to get  [1,2,3,4,100]
-        # count=1
-        # for i in range(1,n):      # start the loop froom and compares its previous value. if its same
+        maxi=1
+        n=len(nums)   # sort the arry to get  [1,2,3,4,100]
+        count=1
+        for i in range(1,n):      # start the loop froom and compares its previous value. if its same
 
-        #     if nums[i]==nums[i-1]:
-        #         continue # if the cuurent element is equal to previous element, go to next iteration and skip below
+            if nums[i]==nums[i-1]:
+                continue # if the cuurent element is equal to previous element, go to next iteration and skip below
 
-        #     if nums[i]==nums[i-1]+1:    # if value at i same as i-1+1. increment count
-        #         count+=1
-        #     else:
-        #         maxi=max(maxi, count)   # Sequence has stopped so update the count with maxi
-        #         count=1    # set the count back to 1 again so that it starts fresh from next itreraiton
+            if nums[i]==nums[i-1]+1:    # if value at i same as i-1+1. increment count
+                count+=1
+            else:
+                maxi=max(maxi, count)   # Sequence has stopped so update the count with maxi
+                count=1    # set the count back to 1 again so that it starts fresh from next itreraiton
 
-        # maxi = max(maxi, count)   # after the final sequence checck again
-        # return maxi
+        maxi = max(maxi, count)   # after the final sequence checck again
+        return maxi
