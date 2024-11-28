e0dfcf6921bf2174b3ec0d290455b09f9b6c0ba5
Kirthikraj Kamaraj <74924366+Kirthik1104@users.noreply.github.com>
2024-11-27 21:08:07 -0500

    Time: 0 ms (100%), Space: 17.4 MB (8.48%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,23 +1,22 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        num_stack = []
-        str_stack = []
-        curr_str = ""
-        curr_num = 0
+        numstack=[]
+        strstack=[]
+        curnum=0
+        curstr=""
 
         for char in s:
-            if char.isdigit():  # Build the current number
-                curr_num = curr_num * 10 + int(char)
-            elif char == '[':  # Push current string and number to stacks
-                num_stack.append(curr_num)
-                str_stack.append(curr_str)
-                curr_str = ""  # Reset for the new scope
-                curr_num = 0
-            elif char == ']':  # Pop from stacks and build the decoded string
-                repeat_times = num_stack.pop()
-                prev_str = str_stack.pop()
-                curr_str = prev_str + (curr_str * repeat_times)
-            else:  # Append character to current string
-                curr_str += char
-
-        return curr_str
+            if char.isdigit():
+                curnum=curnum*10 + int(char)
+            elif char == '[':
+                numstack.append(curnum)
+                strstack.append(curstr)
+                curnum=0
+                curstr=""
+            elif char == ']':
+                times=numstack.pop()
+                previous=strstack.pop()
+                curstr=previous + (curstr*times)
+            else:
+                curstr+=char
+        return curstr
