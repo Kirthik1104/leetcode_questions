196b00fc1d9d7e2bd911b6cb289b8b23e1f9c684
Kirthikraj Kamaraj <74924366+Kirthik1104@users.noreply.github.com>
2024-11-29 18:08:58 -0500

    Time: 19 ms (43.17%), Space: 18.8 MB (6.8%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,21 +1,28 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
         n=len(height)
-        left=[0]*n
-        right=[0]*n
-        res1=0
-        res2=0
+        left=[0]*n             # List to store the max height from left to right
+        right=[0]*n            # List to store the max height from right to left
+        res1=0                 # Variable to store the max height of left
+        res2=0                 # Variable to store the max height of right
 
-        for i in range(n):
-            res1=max(res1, height[i])
-            left[i]=res1
+        for i in range(n):     
+            res1=max(res1, height[i]) # Traversing and storing the max height encountered from left to right
+            left[i]=res1              # Filling max height from left to right
 
-            j=n-i-1
+            j=n-i-1                # n-i-1 : To start the index from right to left
 
-            res2=max(res2, height[j])
-            right[j]=res2
+            res2=max(res2, height[j])  # Traversing and storing the max height encountered from right to left
+            right[j]=res2              # Filling max height from right to left
 
-        result=0
-        for i in range(n):
-            result+=min(left[i], right[i])-height[i]
+        result=0                       
+        for i in range(n):              
+            result+=min(left[i], right[i])-height[i]   # from max height of both array, getting the min height and subtracting it with current height to get the result
         return result
+
+
+        """
+        Tc: 0(n)
+        Sc: 0(n)
+
+        """
