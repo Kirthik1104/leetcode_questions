74cb4192c95112936b28b2e8b2789820c767f441
Kirthikraj Kamaraj <74924366+Kirthik1104@users.noreply.github.com>
2024-11-28 21:16:18 -0500

    Time: 5 ms (39.82%), Space: 18.1 MB (7.52%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,5 +1,12 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+
+        """
+        Time complexity: O(n)
+
+        Since array is already sorted then we no need to create a dic to store the index and val, we can directly use two pointer
+
+        """
         left=0
         right=len(numbers)-1
 
