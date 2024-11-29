640f1619c7461b46cb4b9472745f48b8a9bd5737
Kirthikraj Kamaraj <74924366+Kirthik1104@users.noreply.github.com>
2024-11-29 11:02:15 -0500

    Time: 135 ms (5.01%), Space: 28.2 MB (33.99%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,5 +1,16 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+
+        """
+        Two Pointer: Tc O(n)
+        why Two pointer?
+        ------------------
+        - If height at either pointer position is highest than we will keep it constant and move the other pointer so that we get the matching height or close to highest height.
+
+        width= horizantal distance btw indexes (i, j): area= (j-1)* min(height[left], height[right])
+        why min? because if either height at pointer is smaller than the other one, than the heights are slanted.
+        thats why we take min height of both and multiply it with difference of horizantal distance to get area.
+        """
         left=0
         right=len(height)-1
         res=0
@@ -20,25 +31,25 @@ class Solution:
 
 
 
-#         """
-#         Brute force
-#         ----------
-#         Time complexity: O(n^2)
+        """
+        Brute force
+        ----------
+        Time complexity: O(n^2)
 
 
-#     right-left:- The width is simply the difference between the indices of the two lines, which gives us the horizontal distance.
+    right-left:- The width is simply the difference between the indices of the two lines, which gives us the horizontal distance.
 
-# min(height[left],height[right]):-area is determined by multiplying this width by the shorter height of the two lines, as the container cannot hold more water than the height of the shorter line.
+min(height[left],height[right]):-area is determined by multiplying this width by the shorter height of the two lines, as the container cannot hold more water than the height of the shorter line.
 
-#        """
+       """
 
-#         # res=0
+        # res=0
 
-#         # for left in range(len(height)):               
-#         #     for right in range(left+1, len(height)):
-#         #         area=(right-left)*min(height[left],height[right])
-#         #         res=max(res, area)
-#         # return res
+        # for left in range(len(height)):               
+        #     for right in range(left+1, len(height)):
+        #         area=(right-left)*min(height[left],height[right])
+        #         res=max(res, area)
+        # return res
 
 
 
