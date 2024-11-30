1b056aa360c73ac6fe39c030baa8822d0108aad1
Kirthikraj Kamaraj <74924366+Kirthik1104@users.noreply.github.com>
2024-11-29 20:50:58 -0500

    Update 0121-best-time-to-buy-and-sell-stock.py
    added new code at top with explanation and TC

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -3,6 +3,11 @@ class Solution:
 
         """
         Tc: O(n)
+
+        Sliding window using two pointer, with left at 0 and right at 1
+        Update maxi with profit only if right is greater than left.
+        if left is greater set left equal to right and increment right.
+        Keep incrementing right irrespectuve of anything
         """
         left=0
         right=1
@@ -46,14 +51,14 @@ O(n^2): The nested loops result in a quadratic time complexity, which is ineffic
     Brute
 
         """
-        # maxi=0
-        # n=len(prices) 
-        # for buy in range(n):              #satart at index 0
-        #     for sell in range(buy+1, n):  # start from index+1: because sell should happen on next day
-        #         profit=prices[sell]-prices[buy] # calcualte profit sell - buy
-        #         maxi=max(maxi, profit)       # Update the max profit for every buy and sel
+        maxi=0
+        n=len(prices) 
+        for buy in range(n):              #satart at index 0
+            for sell in range(buy+1, n):  # start from index+1: because sell should happen on next day
+                profit=prices[sell]-prices[buy] # calcualte profit sell - buy
+                maxi=max(maxi, profit)       # Update the max profit for every buy and sel
 
-        # return maxi
+        return maxi
 
 
 """
@@ -62,21 +67,21 @@ Two  pointer
 Time complexity: O(n)
 """
       
-        # left=0
-        # right=1
-        # maxi=0
+        left=0
+        right=1
+        maxi=0
 
-        # while right<len(prices):
+        while right<len(prices):
 
-        #     if prices[left]<prices[right]:   #Check if price at left is less than right      
+            if prices[left]<prices[right]:   #Check if price at left is less than right      
 
-        #         profit=prices[right]-prices[left] # calcualte profit sell - buy
-        #         maxi=max(maxi, profit)          # Update the max profit for every buy and sel
+                profit=prices[right]-prices[left] # calcualte profit sell - buy
+                maxi=max(maxi, profit)          # Update the max profit for every buy and sel
  
-        #     else:                         #if left is greater than right
-        #         left=right                # assign to right(because right is anyways smaller) and shfit right by 1
-        #     right+=1                      # keep shifting right by 1 irrespective of anything
-        # return maxi
+            else:                         #if left is greater than right
+                left=right                # assign to right(because right is anyways smaller) and shfit right by 1
+            right+=1                      # keep shifting right by 1 irrespective of anything
+        return maxi
 
 
 
