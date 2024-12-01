ec2cab1b031091a6652996efd3db2d400e10f432
Kirthikraj Kamaraj <74924366+Kirthik1104@users.noreply.github.com>
2024-12-01 18:09:18 -0500

    Time: 119 ms (48.45%), Space: 32.6 MB (23.31%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -4,9 +4,15 @@ class Solution:
         stack=[]
         result=[0]*n
 
+        """
+        1) For the first iteration the stack stores in intial temp and its index val,
+        2) For second iteration it checks if the temp  at second iteration is > greater than top element present in stack (previous)-> if yes pop the top temp from stack which is lesser than current temp, add the difference to result and add the current temp in stack
+        3) If the temp is smaller than previous one add it to stack, because its a monolithic stack, smaller to larger, so if you find a current, smaller one then previous one add it to stack
+        """
+
         for i, t in enumerate(temperatures):
-            while stack and t>stack[-1][0]:
+            while stack and t>stack[-1][0]:  # 
                 stackt, stacki = stack.pop()
                 result[stacki]=i-stacki
-            stack.append((t, i))       
+            stack.append((t, i))     #
         return result 
