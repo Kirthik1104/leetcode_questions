5735ea85dffcf1c47131d83b3d3419530a48bb8e
Kirthikraj Kamaraj <74924366+Kirthik1104@users.noreply.github.com>
2024-12-02 17:17:26 -0500

    Update 0394-decode-string.py
    added comments

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -8,15 +8,15 @@ class Solution:
         for char in s:
             if char.isdigit():
                 curnum=curnum*10 + int(char)
-            elif char == '[':
+            elif char == '[':                              # Before the opening bracket add all the cur num and curstr to their stack and empty them so that whatever is coming inside this square brackets can be processed further
                 numstack.append(curnum)
                 strstack.append(curstr)
                 curnum=0
                 curstr=""
-            elif char == ']':
-                times=numstack.pop()
+            elif char == ']':                       # Before the closing brackets pop the previous number from stack and store it in times, and pop the previous from strstack and store it in previous and 
+                times=numstack.pop() 
                 previous=strstack.pop()
-                curstr=previous + (curstr*times)
+                curstr=previous + (curstr*times)    # multiply the current char with previous nums(times
             else:
                 curstr+=char
         return curstr
