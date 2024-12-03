1528e78e6193af925b80d7b6269e07741118011c
Kirthikraj Kamaraj <74924366+Kirthik1104@users.noreply.github.com>
2024-12-02 21:28:50 -0500

    Time: 0 ms (100%), Space: 17.4 MB (8.2%) - LeetHub

--- 1095-two-city-scheduling/1095-two-city-scheduling.py
+++ 1095-two-city-scheduling/1095-two-city-scheduling.py
@@ -1,19 +1,12 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-         # Initialize the total cost to 0
-        total_cost = 0
+        costs.sort(key=lambda pair: pair[0]-pair[1])
 
-        # Sort the costs list in ascending order based on the difference
-        # between the costs of sending someone to city A vs city B
-        costs.sort(key=lambda x: x[0] - x[1])
+        n=len(costs)
+        result=0
+        for i in range(n//2):
+            result+=costs[i][0]
+        for i in range(n//2, n):
+            result+=costs[i][1]
+        return result
 
-        # Get the length of the costs list
-        cost_length = len(costs)
-
-        # For each pair of cities, add the cost of sending one person to city A and
-        # the other person to city B to the total cost
-        for i in range(cost_length // 2):
-            total_cost = total_cost + costs[i][0] + costs[cost_length - i - 1][1]
-
-        # Return the total cost
-        return total_cost
