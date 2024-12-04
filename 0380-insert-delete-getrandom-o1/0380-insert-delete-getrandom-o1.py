2ffed57c4427f3b1fb68b1df89113d4050787e94
Kirthikraj Kamaraj <74924366+Kirthik1104@users.noreply.github.com>
2024-12-04 17:05:57 -0500

    Time: 115 ms (67.25%), Space: 57.5 MB (24.35%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -5,18 +5,42 @@ class RandomizedSet:
         self.mylist=[]
         self.hashmap={}
     def insert(self, val: int) -> bool:
-        if val in self.hashmap:
+        if val in self.hashmap:  # if val is already present as keys return False
             return False
         
-        self.hashmap[val]=len(self.mylist)  # For 1st val, self.mylist list is empty so index value would be 0, 2nd time value will be inserted after this statement so for next element length of new list be 1 which would be the index of the second element
+        self.hashmap[val]=len(self.mylist)  # For 1st val, self.mylist list is empty so index value would be 0, value is also appended to mylist, so len of my list is 1. so for next call element length of new list be 1 which would be the index of the second element and so on
         self.mylist.append(val)
         return True
 
         
     def remove(self, val: int) -> bool:
-        if val not in self.hashmap:
+        if val not in self.hashmap:    # If val is not present as keys in hashmap, nothing to remove. return False
             return False
         
+        """
+        To remove val=2
+        ------------
+        mylist=[1,2,3]
+        hashmap={1:0, 2:1, 3:2}
+
+        lastelement= mylist[-1] which gives 3
+        index_to_remove= hashmap[2] which gives the index of val to removed= 1
+
+        mylist[1]=lastelement [1,3,3]--> at position 2 we inserted the same value as the last..now remove last
+        mylist.pop()--> [1,3]
+
+        hashmap[lastelement]=index_to_remove--> since val is removed and last position is changed, will update in hashmap
+        hashmap[3]=0  {1:0, 3:1, 2:1}
+
+        hashmap.pop(val)-->vals is 2-->removes the key and value from hashap since val is also removed from mylist
+
+        mylits[1,3]
+        hashmap={1:0,3:1}
+
+
+
+        """
+
         lastelement=self.mylist[-1]           # To get the last elemnt of the list
         index_to_remove=self.hashmap[val]      # To get the index of the val to be removed
 
