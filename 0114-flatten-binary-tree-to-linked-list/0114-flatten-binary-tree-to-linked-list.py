c93f9aaafbb2a41739899fe14bb6843d20ca80ff
Kirthikraj Kamaraj <74924366+Kirthik1104@users.noreply.github.com>
2024-12-12 02:15:49 -0500

    Time: 0 ms (100%), Space: 17.6 MB (6.37%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
@@ -9,6 +9,12 @@ class Solution:
         """
         Do not return anything, modify root in-place instead.
         """
+
+        """ 
+        The time complexity of the algorithm is O(n) on average, where n is the number of nodes in the tree. This is because each node is visited exactly once.
+
+
+        """
         curr=root
 
         while curr:
@@ -23,5 +29,17 @@ class Solution:
                 curr.left=None
             
             curr=curr.right
+
+        
+        """
+        Why not set left = None for nodes without a left child?
+        --------------------------------------------------------
+        Now, for nodes that don't have a left child:
+
+        Their left pointer is already None by default, because the TreeNode class was initialized with left=None (if no left child is provided).
+        There's no need to set left = None again because it's already None.
+        We only set left = None explicitly when there's an actual left child, and we move that left subtree to the right.
+
+        """
 
         
