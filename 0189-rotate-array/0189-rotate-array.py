7e5d797600788eb619b51ba2d3477a834500a737
Kirthikraj Kamaraj <74924366+Kirthik1104@users.noreply.github.com>
2024-12-16 06:38:55 -0500

    Time: 3 ms (75.62%), Space: 25.2 MB (11.04%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.py
@@ -0,0 +1,32 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+
+        Process:
+        Calculate Effective Rotations:k=k%len(nums)=12%5=2
+        Reasoning:A full rotation happens every len(nums)=5 steps.
+        12 steps include:2 full rotations (2×5=10).
+        2 extra steps.
+        Thus, 12 steps are equivalent to rotating 2 steps to the right.
+        """
+        k=k%len(nums)  #--> if ks is greater than len of nums...just consider the remainder and rotate by that,
+        self.reverse(nums, 0, len(nums)-1)  # Reverse whole array
+        self.reverse(nums, 0, k-1)        #  Reverse from start to k
+        self.reverse(nums, k, len(nums)-1)  # reverse from k to end
+
+        return nums
+
+
+    def reverse(self, nums, start, end): # Swapping code to swap the numbers
+        while start<=end:
+            temp=nums[start]
+            nums[start]=nums[end]
+            nums[end]=temp
+            start+=1
+            end-=1
+        return nums
+
+   
+
+        
