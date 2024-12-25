ebf2735e4ea70ed0e07aa56acfc8bb898ba541fa
Kirthikraj Kamaraj <74924366+Kirthik1104@users.noreply.github.com>
2024-12-25 03:25:11 -0500

    Time: 0 ms (100%), Space: 17.9 MB (5.05%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,9 +1,26 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        m=len(haystack)
-        n=len(needle)
+        m=len(haystack)               # Length og haystack
+        n=len(needle)                 # Length of needle
+        left=0                        # left pointer used to iterate over haystack
+        right=len(needle)             # right pointer (actual length of needle)
+
+        """
+        Creating a slicing or window of size needle. left=0 and right would be length of needle
+        haystack = "sadbutsad", needle = "sad"
+
         left=0
-        right=len(needle)
+        right=len(needle)=3
+
+        - using the window size of needle to check in haystack
+        haystack[left:right] gives haystack[0:3]-->sad.starswith(needle)--> True
+        - if the window size of haystack does not contain string in needle, go to next window by incrementing left and right and checking again
+
+        - If the window size is found return the left pointer which will give the index of the first occurence of string
+
+        - else -1
+
+        """
 
         while right<=len(haystack):
             if not haystack[left:right].startswith(needle):
