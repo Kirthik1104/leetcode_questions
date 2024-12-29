75991b62bd80d6d9b7b960ebe18214c52a5402d1
Kirthikraj Kamaraj <74924366+Kirthik1104@users.noreply.github.com>
2024-12-29 12:12:26 -0500

    Time: 0 ms (100%), Space: 18.1 MB (11.81%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -3,26 +3,25 @@ class Solution:
 
         # Binary Search
 
-        for each_array in matrix:
-        
-            left=0
-            right=len(each_array)-1
+        row=len(matrix)
+        column=len(matrix[0])
 
-            
-    
-            while left<=right:
-                mid=(left + right)//2
-                
-                if each_array[mid]==target:
-                    return True
-                elif target> each_array[mid]:
-                    left = mid+1
-                else:
-                    right = mid-1
+        left=0
+        right= row * column - 1
+        print(right)
+        while left<=right:
+            mid=(left + right)//2
+            print(mid)
+            mid_value = matrix [mid//column][mid%column]
+            print(mid_value)
+
+            if mid_value==target:
+                return True
+            elif target> mid_value:
+                left = mid+1
+            else:
+                right = mid-1
         return False
-
-
-         
 
         # Bruet Force:
 
