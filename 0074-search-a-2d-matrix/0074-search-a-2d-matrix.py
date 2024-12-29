c32d547a70e6e6505d53d635ee4b4f7379fcdcd1
Kirthikraj Kamaraj <74924366+Kirthik1104@users.noreply.github.com>
2024-12-29 12:38:51 -0500

    Time: 0 ms (100%), Space: 18.3 MB (7.24%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,7 +1,24 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
 
-        # Binary Search
+        """
+        1) The idea is to conceptually flattenning the 2d matrix to 1d array and running a while loop from 0 to row * column -1
+
+        2) getting mid point in 1d array, and using this formula to trace the position of element midpoint in the 2d matrix
+
+        3) mid: gives mid value at 1d array. 
+
+              3.1) mid//column---> Gives row number (//) is quotient
+
+              3.2) mid%column--- > Gives the column number (%) is remainder
+
+              why do divide and modulus by column no--> if size of each column is 4 (0,3)
+              index 0//4=0, index 1//4=0, index 2//4=0, index 3//4=0, row=0
+              index 4//4=1, index 5//4=1............................. row=1
+
+              index 0%0= 0 column, index 1%4 = 1 column, index 2%4 = 2 column, index3%4 = 3 column
+
+        """
 
         row=len(matrix)
         column=len(matrix[0])
@@ -12,7 +29,7 @@ class Solution:
         while left<=right:
             mid=(left + right)//2
        
-            mid_value = matrix [mid//column][mid%column]
+            mid_value = matrix [mid//column][mid%column]  # getting row and column of mid
       
 
             if mid_value==target:
