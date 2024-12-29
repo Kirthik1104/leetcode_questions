8ec5a00d0e3f3b21cca5ee8c00eba7228ebab83c
Kirthikraj Kamaraj <74924366+Kirthik1104@users.noreply.github.com>
2024-12-29 09:04:48 -0500

    Time: 0 ms (100%), Space: 18.2 MB (11.81%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,11 +1,33 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
 
-        n=len(matrix)
+        # Binary Search
 
         for each_array in matrix:
-            for each_val in each_array :
-                if each_val==target:
-                    return True
-        return False
         
+            left=0
+            right=len(each_array)-1
+
+            
+    
+            while left<=right:
+                mid=(left + right)//2
+                
+                if each_array[mid]==target:
+                    return True
+                elif target> each_array[mid]:
+                    left = mid+1
+                else:
+                    right = mid-1
+        return False
+
+
+         
+
+        # Bruet Force:
+
+        # for each_array in matrix:
+        #     for each_val in each_array :
+        #         if each_val==target:
+        #             return True
+        # return False
