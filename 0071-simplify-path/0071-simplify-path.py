f2b4ef0f4a5c4b47067ad0948da36a3344d752e8
Kirthikraj Kamaraj <74924366+Kirthik1104@users.noreply.github.com>
2025-01-07 03:55:30 -0500

    Time: 0 ms (100%), Space: 17.7 MB (24.19%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -1,16 +1,22 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        path = path.split("/")
-        directory=[]
+
+        """
+        Summary
+        Time Complexity: O(n)
+        Space Complexity: O(n)
+        """
+        path = path.split("/")  # Split the string with delimeter as / (if // is encountered, it will still assume single  "")--> Result is a list of chars
+        directory=[]           # creating a stack
         for char in path:
-            if char == "" or char == ".":
-                continue
-            elif directory and char == "..":
+            if char == "" or char == ".":  # Acces the list of chars and continue if its a empty space or a single directory
+                continue 
+            elif directory and char == "..":  # check if directory has elements, only then pop the last element when the char is ..
                 directory.pop()
-            elif not directory and char == "..":
+            elif not directory and char == "..": # if dictionary is empty and you encounter a  .. continuue
                 continue
             else:
-                directory.append(char)
+                directory.append(char) # for rest other append (text, ...)
  
-        return ("/"+"/".join(directory))
+        return ("/"+"/".join(directory)) # "/"+ add slash in front, convert the list of chars to a string with / between them and return 
                 
