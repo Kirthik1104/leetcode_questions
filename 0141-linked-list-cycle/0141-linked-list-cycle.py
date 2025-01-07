2fc9d1ce62317ac19a9bc27b27e8b23a9eb52b85
Kirthikraj Kamaraj <74924366+Kirthik1104@users.noreply.github.com>
2025-01-07 13:14:48 -0500

    Time: 54 ms (13.63%), Space: 19.9 MB (24.45%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -8,29 +8,37 @@ class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:    
         """
         Using hashmap
-        """
-        hashmap={}
+        Tc: Thus, the overall time complexity is O(n), where n is the number of nodes in the linked list.
+        Sc: The space required for the hash map is proportional to the number of nodes in the linked list, resulting in a space complexity of O(n).
        
-        while head is not None:
-            if head in hashmap:
+        """
+        # hashmap={}
+       
+        # while head is not None:
+        #     if head in hashmap:  # Step 3: tail's next pointer is in hashmap (cycle detected)---> Return True
+        #         return True
+        #     hashmap[head]=0  # Step 1: From start node 1 to tail create a hashmap with value 0 (Note: Duplicate nodes while travelling from start to end is also unique)
+        #     head=head.next   # Step 2: Traverse till tail node
+        # return False # Step 4: If the tail's node next pointer does not contain any cycle and its pointing to none---> Return False
+
+
+        """
+        Using Fast and slow pointer
+        -------------------------
+        slow pointer: move pointer by 1 position
+        fast pointer: move pointer by 2 position
+        """
+
+        fast_pointer=head
+        slow_pointer=head
+        
+        while fast_pointer is not None and fast_pointer.next is not None:
+            slow_pointer=slow_pointer.next
+            fast_pointer=fast_pointer.next.next
+            if slow_pointer==fast_pointer:
                 return True
-            hashmap[head]=0          
-            head=head.next
         return False
 
-
-        """
-        Using Hashset
-        """
-        # hashset=set()
-        # current=head
-        # while current is not None:
-        #     if current in hashset:
-        #         return True
-        #     hashset.add(current)
-        #     current=current.next
-        # return False
-     
 
 
 
