77baaf2daae1a073f21e3e06a7b285c8bb57ebcd
Kirthikraj Kamaraj <74924366+Kirthik1104@users.noreply.github.com>
2025-01-09 01:48:38 -0500

    Time: 26 ms (31.6%), Space: 17.8 MB (36.75%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -2,17 +2,27 @@ class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         hashzine={}
 
+        """
+        Step 1) Create a hashmap to store the chars of magazine with the count of its occurence. This will be later on used to map the count in ransomNote 
+        """
         for char in magazine:
-            hashzine[char]=hashzine.get(char, 0)+1
-        print(hashzine)
+            hashzine[char]=hashzine.get(char, 0)+1 
+        """
+        Step 2) Now loop the chars in ransomNote, and 
+            a) check first condition: if the char in ranso.... and not in maga... --> return False
+                a.1)  If char exits in ransom... and in maga...
+                        update the count or occurence in hashmap by decrementing by -1
+                        check if the value goes below 0, that means the charcter does not match in either strings
+            If the loop ran till last that means their exits a match 
+        """
         for char in ransomNote:
             if char in hashzine:
-                hashzine[char]-=1
-                if hashzine[char]==-1:
+                hashzine[char]-=1 # if char in hashzine, decrement count by -1
+                if hashzine[char]==-1: # quikcly check if the value is less than 0 if yes, the return False
                     return False
-            else:
+            else:                  # if char exits in ransomNote and does not appears in magazine, return False
                 return False
-        return True
+        return True   # If traversed fully that means the patterns are matching
 
 
 
