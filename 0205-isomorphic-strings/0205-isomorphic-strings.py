9a34558c2d37a64f2515c7bc1f4bc5f99c84fc3d
Kirthikraj Kamaraj <74924366+Kirthik1104@users.noreply.github.com>
2025-01-09 02:22:33 -0500

    Time: 7 ms (58.79%), Space: 17.9 MB (33.59%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,43 +1,69 @@
 from collections import defaultdict
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        #why? 200 ---> maximum ASCII value for characters (including letters and punctuation marks) is 127. bit extended ASCII values we took a safer length of 200
-        array_s=[0]*200  
-        array_t=[0]*200
+        s_ord=[0]*200
+        t_ord=[0]*200
 
-        
-        length_s=len(s) 
-        length_t=len(t)
-
-        # if the length of t and s are not same then return False
-        if length_s!=length_t:
-            return False
-
-        """
-        for each character go to array and update the count
-        for i=0      e                             a
-        array_s = [_ 1 _ _ _ _ _ _ _ ]  array_t = [1 _ _ _ _ _ _ _ _ ]
-
-        for i=1      e         g                   a     d
-        array_s = [_ 1 _ _ _ _ 2 _ _ ]  array_t = [1 _ _ 2 _ _ _ _ _ ]
-
-        for i=2      e         g                   a     d
-        array_s = [_ 1 _ _ _ _ 3 _ _ ]  array_t = [1 _ _ 3 _ _ _ _ _ ]
-
-        since the of g and d are same, it is isomorphic. Update the count
-        
-        
-        
-        """
-        for i in range(length_s):
-            if array_s[ord(s[i])]!=array_t[ord(t[i])]:
+        for index in range(len(t)):
+            if s_ord[ord(s[index])]!= t_ord[ord(t[index])] :
                 return False
-
-
-            array_s[ord(s[i])]=i+1
-            array_t[ord(t[i])]=i+1
-        
+                
+            s_ord[ord(s[index])]= index+1
+            t_ord[ord(t[index])]= index+1
         return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # #why? 200 ---> maximum ASCII value for characters (including letters and punctuation marks) is 127. bit extended ASCII values we took a safer length of 200
+        # array_s=[0]*200  
+        # array_t=[0]*200
+
+        
+        # length_s=len(s) 
+        # length_t=len(t)
+
+        # # if the length of t and s are not same then return False
+        # if length_s!=length_t:
+        #     return False
+
+        # """
+        # for each character go to array and update the count
+        # for i=0      e                             a
+        # array_s = [_ 1 _ _ _ _ _ _ _ ]  array_t = [1 _ _ _ _ _ _ _ _ ]
+
+        # for i=1      e         g                   a     d
+        # array_s = [_ 1 _ _ _ _ 2 _ _ ]  array_t = [1 _ _ 2 _ _ _ _ _ ]
+
+        # for i=2      e         g                   a     d
+        # array_s = [_ 1 _ _ _ _ 3 _ _ ]  array_t = [1 _ _ 3 _ _ _ _ _ ]
+
+        # since the of g and d are same, it is isomorphic. Update the count
+        
+        
+        
+        # """
+        # for i in range(length_s):
+        #     if array_s[ord(s[i])]!=array_t[ord(t[i])]:
+        #         return False
+
+
+        #     array_s[ord(s[i])]=i+1
+        #     array_t[ord(t[i])]=i+1
+        
+        # return True
 
 
         """
