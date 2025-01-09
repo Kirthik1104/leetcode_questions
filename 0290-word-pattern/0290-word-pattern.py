b63a73b591d8426c7716c5b2e1b63ecbb0525d9a
Kirthikraj Kamaraj <74924366+Kirthik1104@users.noreply.github.com>
2025-01-09 02:47:44 -0500

    Time: 0 ms (100%), Space: 18 MB (5.74%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,6 +1,40 @@
 from collections import defaultdict
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
+        word=s.split(' ')
+        hashmap={}
+        if len(pattern)!=len(word):
+            return False
+        
+       #set will give pattern as 'a' and keep dog and cat as  and their is not equal
+        
+        if len(set(pattern))!=len(set(word)): 
+            return False
+        
+        for ind in range(len(pattern)):
+            if pattern[ind] not in hashmap:
+                hashmap[pattern[ind]] = word[ind]
+                
+            elif hashmap[pattern[ind]]!=word[ind]:
+              
+                return False
+        return True
+
+
+
+
+
+
+
+
+
+
+
+        """
+        Space: O(n) - scan
+        Time: O(n) - for the hashmap
+
+        """
         new_words=s.split(' ')  #----> Converting string of words to list of words
 
         if len(pattern)!=len(new_words):   # if length of pattern and length of new_words does not match return False
@@ -10,7 +44,7 @@ class Solution:
         pattern="aa"
         s="dog, cat"
 
-        set will give pattern as 'a' and keep dog and cat as as and their is not equal
+        set will give pattern as 'a' and keep dog and cat as  and their is not equal
         """
         if len(set(pattern))!=len(set(new_words)): 
             return False
