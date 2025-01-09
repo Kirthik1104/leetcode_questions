f9af88e97926fb993f72b653d20ef60247a5ebf6
Kirthikraj Kamaraj <74924366+Kirthik1104@users.noreply.github.com>
2025-01-09 10:17:06 -0500

    Time: 0 ms (100%), Space: 17.6 MB (32.12%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,5 +1,8 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         # Tc: O(n): split O(N), -1 O(1), len(lastword) is O(k). Overall O(n)
+
         return len(s.split()[-1])   #--> Split: Removes all space and create a list of words. -1 access last word, len gives length of last word
+
+        # for empty s=""  (input is greater than 0r equal to 1) but in this case put a if condition: if not s: return False
         
