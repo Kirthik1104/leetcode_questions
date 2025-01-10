e38a6fb3ecae121d594b0c00aa1078cb26cfc91a
Kirthikraj Kamaraj <74924366+Kirthik1104@users.noreply.github.com>
2025-01-10 11:26:36 -0500

    Time: 0 ms (100%), Space: 17.8 MB (21.27%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -5,18 +5,54 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        # dummy node is created to better deal with edge cases of removing first node, tail node.
         dummy=ListNode(0, head)
         first=dummy
         second=dummy
 
-        for iterare in range(n+1): # n+1, because we are starting with dummy node
+        for _ in range(n):
             second=second.next
         
-        while second is not None:
-            second=second.next
+        while second.next:
             first=first.next
+            second=second.next
         
         first.next=first.next.next
-    
+
         return dummy.next
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        # # dummy node is created to better deal with edge cases of removing first node, tail node.
+        # dummy=ListNode(0, head)
+        # first=dummy
+        # second=dummy
+
+        # for iterare in range(n+1): # n+1, because we are starting with dummy node
+        #     second=second.next
+        
+        # while second is not None:
+        #     second=second.next
+        #     first=first.next
+        
+        # first.next=first.next.next
+    
+        # return dummy.next
