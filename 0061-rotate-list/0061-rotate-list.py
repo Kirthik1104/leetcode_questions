216e7dc6643a7dae14cb9bbcb34ffa0f4299a412
Kirthikraj Kamaraj <74924366+Kirthik1104@users.noreply.github.com>
2025-01-12 11:09:48 -0500

    Time: 0 ms (100%), Space: 17.8 MB (23.42%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.py
+++ 0061-rotate-list/0061-rotate-list.py
@@ -5,19 +5,25 @@
 #         self.next = next
 class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+
+        """
+        Tc: O(n)
+        """
         if not head:
             return None
         
         lastnode=head
-        length=1
+        length=1         # Getting the length of the linked list
 
-        while lastnode.next:
+        while lastnode.next:  # Just stopping exactly before the None--> At last Node
             lastnode=lastnode.next
             length+=1
+        
         
         k=k%length  # if k is greater than length of node's list then, this will give remainder (how many extra k is required after 1 full rotatio. (full rotation is assumed))
         # print(k)
         
+        #Connect the lastnode with head---> Creating a circular linked list
         lastnode.next=head # lastnode points to 1 node before none. point lastnode.next to head----> to create a circular linked list
 
         tempnode=head
@@ -27,10 +33,15 @@ class Solution:
         k=2
         5-2-1=2 : To point to excalt 1 node before the rotation starts
         """
-        for i in range(length-k-1): 
+        for i in range(length-k-1): # stop one node before the rotation starts
             tempnode=tempnode.next
         
-        answer=tempnode.next
+        """
+        [1,2,3,4,5]
+        answer= 4,5,1,2,3 --> Since its a circular linked list
+        4,5,1,2,3->None  -->tempnode.next
+        """
+        answer=tempnode.next #
         tempnode.next=None
 
         return answer
