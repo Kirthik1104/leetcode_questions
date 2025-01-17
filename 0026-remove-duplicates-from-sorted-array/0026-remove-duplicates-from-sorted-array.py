012502c4d4f88920d91774b53d939e398c3eb4a0
Kirthikraj Kamaraj <74924366+Kirthik1104@users.noreply.github.com>
2025-01-17 13:56:37 -0500

    Time: 3 ms (49.85%), Space: 19 MB (16.74%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,25 +1,57 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        """
-        1) j: will start from second index
-        2) i: will also start from second index
 
-        Eg:1
-        k=1
-        for loop i=1
-        [1,2,3,4]:
-        index 1!=index 0
-        2!=1 
-        nums[k] which is 1 is equal to nums[i] whichs is also 1, so same value 
+        i=0
+        j=0
 
-        nums[k] which is 2
+        while j<len(nums):
+            if nums[i]!=nums[j]:
+                nums[i+1]=nums[j]
+                i+=1
+            j+=1
+        return i+1
 
-        """
-        j=1                                 # Pointer to keep the index of second duplicate till we find the unique element
-        for i in range(1, len(nums)):
-            if nums[i]!=nums[i-1]:              # check i with 0
-                nums[j]=nums[i]          # update the pointer with unique elements
-                j+=1                     # increment pinter
-        return j                       # return j
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#         """
+#         1) j: will start from second index
+#         2) i: will also start from second index
+
+#         Eg:1
+#         k=1
+#         for loop i=1
+#         [1,2,3,4]:
+#         index 1!=index 0
+#         2!=1 
+#         nums[k] which is 1 is equal to nums[i] whichs is also 1, so same value 
+
+#         nums[k] which is 2
+
+#         """
+#         j=1                                 # Pointer to keep the index of second duplicate till we find the unique element
+#         for i in range(1, len(nums)):
+#             if nums[i]!=nums[i-1]:              # check i with 0
+#                 nums[j]=nums[i]          # update the pointer with unique elements
+#                 j+=1                     # increment pinter
+#         return j                       # return j
 
         
