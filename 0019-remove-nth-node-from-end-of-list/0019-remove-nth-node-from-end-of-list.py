fb651d832ae3a8d625690ca89b8b623b927fede9
Kirthikraj Kamaraj <74924366+Kirthik1104@users.noreply.github.com>
2025-01-16 22:57:38 -0500

    Time: 0 ms (100%), Space: 17.9 MB (7%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -5,24 +5,19 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+
         dummy=ListNode(0, head)
-        first=dummy
-        second=dummy
+        curr=dummy
+        prev=dummy
 
-        # Move the second pointer by n steps
-        for _ in range(n):
-            second=second.next
+        for iterate in range(n):
+            curr=curr.next
         
-       # Start moving each node ahead by 1 position, the first pointer will stop 1 node before the n node which has to be removed
-        # the second pointer will stop exactly before the none
-        while second.next:
-            first=first.next
-            second=second.next
+        while curr.next:
+            curr=curr.next
+            prev=prev.next
         
-        # skip the nth node
-        first.next=first.next.next
-
-        # return the head
+        prev.next=prev.next.next
         return dummy.next
 
 
@@ -45,19 +40,62 @@ class Solution:
 
 
 
-        
-        # # dummy node is created to better deal with edge cases of removing first node, tail node.
-        # dummy=ListNode(0, head)
-        # first=dummy
-        # second=dummy
 
-        # for iterare in range(n+1): # n+1, because we are starting with dummy node
-        #     second=second.next
+
+#         #dummynode with value 0, and next is pointing to head, check the ListNode constructor
+#         dummy=ListNode(0, head)
+#         first=dummy    
+#         second=dummy
+
+#         # Move the second pointer by n steps
+#         for _ in range(n):
+#             second=second.next
         
-        # while second is not None:
-        #     second=second.next
-        #     first=first.next
+#        # Start moving each node ahead by 1 position, the first pointer will stop 1 node before the n node which has to be removed
+#         # the second pointer will stop exactly before the none
+#         while second.next:
+#             first=first.next
+#             second=second.next
         
-        # first.next=first.next.next
+#         # skip the nth node
+#         first.next=first.next.next
+
+#         # return the head
+#         return dummy.next
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+#         # # dummy node is created to better deal with edge cases of removing first node, tail node.
+#         # dummy=ListNode(0, head)
+#         # first=dummy
+#         # second=dummy
+
+#         # for iterare in range(n+1): # n+1, because we are starting with dummy node
+#         #     second=second.next
+        
+#         # while second is not None:
+#         #     second=second.next
+#         #     first=first.next
+        
+#         # first.next=first.next.next
     
-        # return dummy.next
+#         # return dummy.next
