b658f5d56a4259e7e4dd868df12ff15a001a1dfa
Kirthikraj Kamaraj <74924366+Kirthik1104@users.noreply.github.com>
2025-01-18 11:01:31 -0500

    Time: 43 ms (96.51%), Space: 17.7 MB (33.06%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -1,16 +1,25 @@
 class Solution:
     def removeDuplicates(self, nums:[list])->int:
-        count=1
+        """
+        1) count is initalised to 1
+        i and j are also intitalised to 1 because the first number will always be valid
+
+        step 1: To check if nums at i is equal to i-1, if yes then increase the occurence count, you have to include occurence count in the result till it<=2
+        step 2: j pointer: Tracks where the next valid element should be written
+        step 3: increment j till occurence lount is less than 2 and it count increases then the if condition will avoid including that number.
+
+        """
+        occurence=1
         i=1
         j=1
-        for i in range(1, len(nums)):
+        for i in range(1, len(nums)): # c
             if nums[i]==nums[i-1]:
-                count+=1
+                occurence+=1
             else:
-                count=1
+                occurence=1
             
 
-            if count<=2:
+            if occurence<=2:
                 nums[j]=nums[i]
                 j+=1
             
