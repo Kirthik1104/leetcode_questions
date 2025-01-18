6fcaac55edba3574165a0234d2d76df4003b7228
Kirthikraj Kamaraj <74924366+Kirthik1104@users.noreply.github.com>
2025-01-18 11:25:33 -0500

    Time: 135 ms (20.98%), Space: 26.9 MB (43.51%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,19 +1,18 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
 
-        """
-        Tc: O(n)
-        """
-        l=0
-        r=1
+        left=0
+        right=1
         maxi=0
-        while r<len(prices):
-            if prices[l]<prices[r]:
-                profit=prices[r]-prices[l]
+        while right<len(prices):
+
+            if prices[left]<=prices[right]:
+                profit= prices[right] - prices[left]
                 maxi=max(maxi, profit)
+                right+=1
             else:
-                l=r
-            r+=1 
+                left+=1
+            
         return maxi
 
 
@@ -38,24 +37,39 @@ class Solution:
 
 
 
+#         """
+#         Tc: O(n)
+#         """
+#         l=0
+#         r=1
+#         maxi=0
+#         while r<len(prices):
+#             if prices[l]<prices[r]:
+#                 profit=prices[r]-prices[l]
+#                 maxi=max(maxi, profit)
+#             else:
+#                 l=r
+#             r+=1 
+#         return maxi
 
 
-        # left=0
-        # right=1
-        # maxi=0
 
-        # while right<len(prices):  # if right reaches end that means all prices are traversed
-        #     if prices[left]<prices[right]: # if prices at left is less than right
-        #         profit=prices[right]-prices[left] #calculate profit
-        #         maxi=max(maxi, profit) #Update the maxi
+
+
+#         # left=0
+#         # right=1
+#         # maxi=0
+
+#         # while right<len(prices):  # if right reaches end that means all prices are traversed
+#         #     if prices[left]<prices[right]: # if prices at left is less than right
+#         #         profit=prices[right]-prices[left] #calculate profit
+#         #         maxi=max(maxi, profit) #Update the maxi
                 
-        #     else:         # else condition when left is > right (buy cannot be greater)
-        #         left=right #setting left index to right index and always incrementing right to 1 
+#         #     else:         # else condition when left is > right (buy cannot be greater)
+#         #         left=right #setting left index to right index and always incrementing right to 1 
 
-        #     right+=1  #even if right is greater of left is greater keep shifting the right till end
-        # return maxi
-
-
+#         #     right+=1  #even if right is greater of left is greater keep shifting the right till end
+#         # return maxi
 
 
 
@@ -74,45 +88,47 @@ class Solution:
 
 
 
-        """
-Time Complexity:
-
-O(n^2): The nested loops result in a quadratic time complexity, which is inefficient for large inputs.
-
-    Brute
-
-        """
-        # maxi=0
-        # n=len(prices) 
-        # for buy in range(n):              #satart at index 0
-        #     for sell in range(buy+1, n):  # start from index+1: because sell should happen on next day
-        #         profit=prices[sell]-prices[buy] # calcualte profit sell - buy
-        #         maxi=max(maxi, profit)       # Update the max profit for every buy and sel
-
-        # return maxi
 
 
-"""
-Two  pointer
+#         """
+# Time Complexity:
 
-Time complexity: O(n)
-"""
+# O(n^2): The nested loops result in a quadratic time complexity, which is inefficient for large inputs.
+
+#     Brute
+
+#         """
+#         # maxi=0
+#         # n=len(prices) 
+#         # for buy in range(n):              #satart at index 0
+#         #     for sell in range(buy+1, n):  # start from index+1: because sell should happen on next day
+#         #         profit=prices[sell]-prices[buy] # calcualte profit sell - buy
+#         #         maxi=max(maxi, profit)       # Update the max profit for every buy and sel
+
+#         # return maxi
+
+
+# """
+# Two  pointer
+
+# Time complexity: O(n)
+# """
       
-        # left=0
-        # right=1
-        # maxi=0
+#         # left=0
+#         # right=1
+#         # maxi=0
 
-        # while right<len(prices):
+#         # while right<len(prices):
 
-        #     if prices[left]<prices[right]:   #Check if price at left is less than right      
+#         #     if prices[left]<prices[right]:   #Check if price at left is less than right      
 
-        #         profit=prices[right]-prices[left] # calcualte profit sell - buy
-        #         maxi=max(maxi, profit)          # Update the max profit for every buy and sel
+#         #         profit=prices[right]-prices[left] # calcualte profit sell - buy
+#         #         maxi=max(maxi, profit)          # Update the max profit for every buy and sel
  
-        #     else:                         #if left is greater than right
-        #         left=right                # assign to right(because right is anyways smaller) and shfit right by 1
-        #     right+=1                      # keep shifting right by 1 irrespective of anything
-        # return maxi
+#         #     else:                         #if left is greater than right
+#         #         left=right                # assign to right(because right is anyways smaller) and shfit right by 1
+#         #     right+=1                      # keep shifting right by 1 irrespective of anything
+#         # return maxi
 
 
 
