1792ff6fed85567a8b970a5eba08c6cc5b9ab089
Kirthikraj Kamaraj <74924366+Kirthik1104@users.noreply.github.com>
2025-01-20 15:57:30 -0500

    Time: 9 ms (55.96%), Space: 18.8 MB (24.83%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -6,9 +6,15 @@ class Solution:
         jump=0
         for i in range(n-1):
             far=max(far, i+nums[i])
-
+            
             if i==end:
                 end=far
                 jump+=1
 
         return jump
+
+
+        """
+        For nums = [2, 0, 0, 1, 4], it is impossible to reach the last index. This violates the problem's guarantee that a solution exists. If this input
+        were valid, it would represent an edge case the algorithm is designed to handle gracefully.
+        """
