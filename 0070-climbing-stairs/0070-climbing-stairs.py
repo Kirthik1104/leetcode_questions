0e98b8c87b216112cb643db07d0d543251d77e38
Kirthikraj Kamaraj <74924366+Kirthik1104@users.noreply.github.com>
2025-01-21 17:01:04 -0500

    Time: 0 ms (100%), Space: 17.8 MB (29.92%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,6 +1,22 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        memo={1:1, 2:2}
+        # Used for memoization, so that if we encounter same values, acces from this dictionary
+
+        """
+        why 1, 2 and not 0, 1---> To reach step 1 we need 1 step or way, to reach step 2, we can directly go to step 2 or go 1+1step. basically finding n ways to reach a step
+        
+        And anything greater than 2--> we will start exploring the permutation from their and store the results in hashmap
+
+        when 1 and 2 are in memo
+        when n=2 the answer should be 2---> 1+1 or directly 2, read description
+
+        if only 1 and 0 are in memo,
+        then n=2 will give 1--> which is wrong as per description
+
+        Tc: O(n)
+        """
+        memo={1:1, 2:2} 
+        # And frp,
 
         def fib(x):
             if x in memo:
