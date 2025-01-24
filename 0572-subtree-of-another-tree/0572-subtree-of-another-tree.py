3ae4fc6e057e99f1bed242bf5fbc033bc1dac7be
Kirthikraj Kamaraj <74924366+Kirthik1104@users.noreply.github.com>
2025-01-24 12:12:35 -0500

    Time: 167 ms (5.02%), Space: 18.2 MB (26.56%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -6,34 +6,34 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        # def dfs(p, q):
-        #     print("hi")
-        #     if not p and not q:
-        #         return True
-        #     if (p and not q) or (q and not p):
-        #         return False
-        #     if p.val!=q.val:
-        #         return False
+        def dfs(p, q):
+            print("hi")
+            if not p and not q:
+                return True
+            if (p and not q) or (q and not p):
+                return False
+            if p.val!=q.val:
+                return False
             
-        #     left = dfs(p.left, q.left)
-        #     right= dfs(p.right, q.right)
+            left = dfs(p.left, q.left)
+            right= dfs(p.right, q.right)
 
-        #     return left and right
+            return left and right
 
        
-        # if not subRoot:
-        #     return True
-        # if not root:
-        #     return False
+        if not subRoot:
+            return True
+        if not root:
+            return False
         
-        # if root.val==subRoot.val:
-        #     if dfs(root, subRoot):
-        #         return True
+        if root.val==subRoot.val:
+            if dfs(root, subRoot):
+                return True
               
-        # # self.isSubtree(root.left, subRoot)
-        # # self.isSubtree(root.right, subRoot)
+        # self.isSubtree(root.left, subRoot)
+        # self.isSubtree(root.right, subRoot)
 
-        # return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
       
 
 
@@ -52,31 +52,31 @@ class Solution:
 
 
 
-        if not subRoot:
-            return True
-        if not root:
-            return False
+    #     if not subRoot:
+    #         return True
+    #     if not root:
+    #         return False
         
-        if self.issame(root, subRoot):
-            return True
+    #     if self.issame(root, subRoot):
+    #         return True
 
      
-        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+    #     return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
 
 
-    def issame(self, root:Optional[TreeNode]=None, subRoot:Optional[TreeNode]=None)->bool:
+    # def issame(self, root:Optional[TreeNode]=None, subRoot:Optional[TreeNode]=None)->bool:
 
-        if root is None and subRoot is None:
-            return True
-        if (root and not subRoot) or (subRoot and not root):
-            return False
-        if root.val!=subRoot.val:
-            return False
+    #     if root is None and subRoot is None:
+    #         return True
+    #     if (root and not subRoot) or (subRoot and not root):
+    #         return False
+    #     if root.val!=subRoot.val:
+    #         return False
         
-        left=self.issame(root.left, subRoot.left)
-        right=self.issame(root.right, subRoot.right)
+    #     left=self.issame(root.left, subRoot.left)
+    #     right=self.issame(root.right, subRoot.right)
 
-        return left and right
+    #     return left and right
 
 
        
